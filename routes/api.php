6dc63d0204a820587c6a5c663e7cd3a641907730
rimer rosa herrera <201809665@est.umss.edu>
2023-09-11 19:35:38 -0400

    version-final

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,15 @@ use App\Http\Controllers\Reserva1Controller;
 use App\Http\Controllers\Reserva2Controller;
 use App\Http\Controllers\Reserva3Controller;
 use App\Http\Controllers\ComunicadoController;
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+});
 
 
 Route::get('reservas/dia-actual', [ReservaController::class, 'reservasDelDia']);
